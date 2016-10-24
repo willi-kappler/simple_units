ec2e869c133428e4dadd2d9f97b4dc826b7866ce
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-10-24 22:54:36 +0200

    refactoring, improve macro, add more units

--- src/si_units.rs
+++ src/si_units.rs
@@ -71,7 +71,11 @@ macro_rules! init_unit {
     }
 }
 
-macro_rules! mul_unit {
+macro_rules! mul_div_unit {
+    // $i1 * $i2 = $i3
+    // $i2 * $i1 = $i3
+    // $i1 = $i3 / $i2
+    // $i2 = $i3 / $i1
     ($i1:ident, $i2:ident, $i3:ident) => {
         impl Mul<$i2> for $i1 {
             type Output = $i3;
@@ -81,36 +85,102 @@ macro_rules! mul_unit {
                 $i3(lhs * rhs)
             }
         }
-    }
-}
 
-macro_rules! div_unit {
-    ($i1:ident, $i2:ident, $i3:ident) => {
-        impl Div<$i2> for $i1 {
+        impl Mul<$i1> for $i2 {
             type Output = $i3;
 
-            fn div(self: $i1, $i2(rhs): $i2) -> $i3 {
-                let $i1(lhs) = self;
-                $i3(lhs / rhs)
+            fn mul(self: $i2, $i1(rhs): $i1) -> $i3 {
+                let $i2(lhs) = self;
+                $i3(lhs * rhs)
             }
         }
-    }
+
+        impl Div<$i2> for $i3 {
+            type Output = $i1;
+
+            fn div(self: $i3, $i2(rhs): $i2) -> $i1 {
+                let $i3(lhs) = self;
+                $i1(lhs / rhs)
+            }
+        }
+
+        impl Div<$i1> for $i3 {
+            type Output = $i2;
+
+            fn div(self: $i3, $i1(rhs): $i1) -> $i2 {
+                let $i3(lhs) = self;
+                $i2(lhs / rhs)
+            }
+        }
+    };
+
+    // $i1 * $i1 = $i2
+    // $i1 = $i2 / $i1
+    ($i1:ident, $i2:ident) => {
+        impl Mul<$i1> for $i1 {
+            type Output = $i2;
+
+            fn mul(self: $i1, $i1(rhs): $i1) -> $i2 {
+                let $i1(lhs) = self;
+                $i2(lhs * rhs)
+            }
+        }
+
+        impl Div<$i1> for $i2 {
+            type Output = $i1;
+
+            fn div(self: $i2, $i1(rhs): $i1) -> $i1 {
+                let $i2(lhs) = self;
+                $i1(lhs / rhs)
+            }
+        }
+    };
 }
 
 init_unit!(Meter);
 
 init_unit!(Meter2);
 
-mul_unit!(Meter, Meter, Meter2);
+init_unit!(Meter3);
+
+mul_div_unit!(Meter, Meter2);
+
+mul_div_unit!(Meter, Meter2, Meter3);
 
 init_unit!(Second);
 
 init_unit!(Second2);
 
-mul_unit!(Second, Second, Second2);
+init_unit!(Second3);
+
+mul_div_unit!(Second, Second2);
+
+mul_div_unit!(Second, Second2, Second3);
 
 init_unit!(MeterPerSecond);
 
-div_unit!(Meter, Second, MeterPerSecond);
+mul_div_unit!(MeterPerSecond, Second, Meter);
 
-mul_unit!(MeterPerSecond, Second, Meter);
+init_unit!(MeterPerSecond2);
+
+mul_div_unit!(MeterPerSecond2, Second, MeterPerSecond);
+
+mul_div_unit!(MeterPerSecond2, Second2, Meter);
+
+init_unit!(Meter2PerSecond);
+
+mul_div_unit!(MeterPerSecond, Meter, Meter2PerSecond);
+
+init_unit!(Meter2PerSecond2);
+
+mul_div_unit!(Meter2PerSecond2, Second, Meter2PerSecond);
+
+mul_div_unit!(Meter2PerSecond2, Second2, Meter2);
+
+mul_div_unit!(MeterPerSecond, Meter2PerSecond2);
+
+init_unit!(KG);
+
+init_unit!(Newton);
+
+mul_div_unit!(KG, MeterPerSecond2, Newton);
