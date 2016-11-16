d26533cdc1ba64745cf7380db89d4fe5cc80c374
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-11-16 23:30:50 +0100

    macro for conversions, more tests

--- src/conversion.rs
+++ src/conversion.rs
@@ -8,6 +8,23 @@ use std::cmp::PartialEq;
 
 use ::si_units::*;
 
+macro_rules! convert_unit {
+    ($i1:ident, $i2:ident, $e1:expr) => {
+        impl From<$i1> for $i2 {
+            fn from($i1(value): $i1) -> Self {
+                $i2(value * $e1)
+            }
+        }
+
+        impl From<$i2> for $i1 {
+            fn from($i2(value): $i2) -> Self {
+                $i1(value / $e1)
+            }
+        }
+    }
+}
+
+
 const METER_TO_FOOT: f64 = 3.28084;
 const METER_TO_YARD: f64 = 1.09361;
 const METER_TO_MILE: f64 = 0.000621371;
@@ -16,20 +33,6 @@ init_unit!(Foot);
 init_unit!(Yard);
 init_unit!(Mile);
 
-impl From<Meter> for Foot {
-    fn from(Meter(value): Meter) -> Self {
-        Foot(value * METER_TO_FOOT)
-    }
-}
-
-impl From<Meter> for Yard {
-    fn from(Meter(value): Meter) -> Self {
-        Yard(value * METER_TO_YARD)
-    }
-}
-
-impl From<Meter> for Mile {
-    fn from(Meter(value): Meter) -> Self {
-        Mile(value * METER_TO_MILE)
-    }
-}
+convert_unit!(Meter, Foot, METER_TO_FOOT);
+convert_unit!(Meter, Yard, METER_TO_YARD);
+convert_unit!(Meter, Mile, METER_TO_MILE);
--- tests/conversion.rs
+++ tests/conversion.rs
@@ -6,5 +6,21 @@ use simple_units::conversion::*;
 #[test]
 fn test_meter_to_foot() {
     assert_eq!((Foot::from(Meter(1.0))), Foot(3.28084));
+
     let value: Foot = Meter(1.0).into();
     assert_eq!(value, Foot(3.28084));
+
+    let value = Foot(1.0) + Meter(1.0).into();
+    assert_eq!(value, Foot(4.2808399999999995));
+}
+
+#[test]
+fn test_foot_to_meter() {
+    assert_eq!((Meter::from(Foot(3.28084))), Meter(1.0));
+
+    let value: Meter = Foot(3.28084).into();
+    assert_eq!(value, Meter(1.0));
+
+    let value = Meter(1.0) + Foot(3.28084).into();
+    assert_eq!(value, Meter(2.0));
+}
