727d10df836123801a7fa1db14cdc44589db1aa7
Willi Kappler <willi.kappler.gm@gmail.com>
2016-11-19 13:22:12 +0100

    mor test cases

--- tests/lib.rs
+++ tests/lib.rs
@@ -12,19 +12,21 @@ fn test_units1() {
     let energy = Joule(105.0);
     let univ_gas_const = JoulePerKelvinMol(8.3144598);
 
-    let temperature1 = DegC(500.0);
-    let temperature2 = DegC(450.0);
+    let temperature1 = DegC(200.0);
+    let temperature2 = DegC(300.0);
 
     let time1 = Second::from(MilYear(1.0));
     let time2 = Second::from(MilYear(2.0));
 
     let cooling_rate = Kelvin::from((temperature2 - temperature1)) / (time2 - time1);
 
-    let temperature_k = Kelvin::from(temperature1);
+    let temperature_k = Kelvin::from(temperature2);
 
     let tau: SecondPerMol = univ_gas_const * (temperature_k * temperature_k) / (energy * cooling_rate);
 
     let geometry_factor = Mol(27.0);
 
     let closure_temp = DegC::from(energy / (Mol((geometry_factor * tau * diff).ln()) * univ_gas_const));
+
+    // assert_eq!(closure_temp, DegC(1.0));
 }
--- tests/si_units.rs
+++ tests/si_units.rs
@@ -13,11 +13,6 @@ fn test_meter_sub() {
 }
 
 #[test]
-fn test_meter_mul_meter() {
-    assert_eq!(Meter(2.5) * Meter(4.0), Meter2(10.0));
-}
-
-#[test]
 fn test_meter_mul_number() {
     assert_eq!(Meter(3.0) * 4.0, Meter(12.0));
 }
@@ -38,18 +33,74 @@ fn test_meter_div_number() {
 }
 
 #[test]
-fn meter2_div_meter() {
-    assert_eq!(Meter2(12.0) / Meter(3.0), Meter(4.0));
-}
+fn test_power3_unit() {
+    assert_eq!(Meter(2.5) * Meter(3.0), Meter2(7.5));
+    assert_eq!(Meter(2.0) * Meter2(3.0), Meter3(6.0));
 
-#[test]
-fn meter3_div_meter() {
-    assert_eq!(Meter3(18.0) / Meter(3.0), Meter2(6.0));
-}
+    assert_eq!(Meter2(1.0) * Meter(3.0), Meter3(3.0));
 
-#[test]
-fn meter3_div_meter2() {
-    assert_eq!(Meter3(24.0) / Meter2(4.0), Meter(6.0));
+    assert_eq!(PerMeter(2.5) * PerMeter(3.0), PerMeter2(7.5));
+    assert_eq!(PerMeter(1.5) * PerMeter2(3.0), PerMeter3(4.5));
+
+    assert_eq!(PerMeter2(2.0) * PerMeter(3.0), PerMeter3(6.0));
+
+    assert_eq!(Meter(10.0) / Meter(5.0), 2.0);
+    assert_eq!(Meter(8.0) / Meter2(4.0), PerMeter(2.0));
+    assert_eq!(Meter(10.0) / Meter3(4.0), PerMeter2(2.5));
+
+    assert_eq!(Meter2(1.0) / Meter(2.0), Meter(0.5));
+    assert_eq!(Meter2(3.0) / Meter2(3.0), 1.0);
+    assert_eq!(Meter2(4.5) / Meter3(1.5), PerMeter(3.0));
+
+    assert_eq!(Meter3(1.0) / Meter(1.0), Meter2(1.0));
+    assert_eq!(Meter3(5.0) / Meter2(2.0), Meter(2.5));
+    assert_eq!(Meter3(8.0) / Meter3(4.0), 2.0);
+
+    assert_eq!(Meter(5.0) * PerMeter(2.0), 10.0);
+    assert_eq!(Meter(2.0) * PerMeter2(3.0), PerMeter(6.0));
+    assert_eq!(Meter(3.1) * PerMeter3(2.0), PerMeter2(6.2));
+
+    assert_eq!(Meter2(1.5) * PerMeter(2.0), Meter(3.0));
+    assert_eq!(Meter2(1.8) * PerMeter2(2.0), 3.6);
+    assert_eq!(Meter2(5.0) * PerMeter3(3.0), PerMeter(15.0));
+
+    assert_eq!(Meter3(3.5) * PerMeter(2.0), Meter2(7.0));
+    assert_eq!(Meter3(3.0) * PerMeter2(2.0), Meter(6.0));
+    assert_eq!(Meter3(6.0) * PerMeter3(3.0), 18.0);
+
+    assert_eq!(Meter(16.0) / PerMeter(4.0), Meter2(4.0));
+    assert_eq!(Meter(9.0) / PerMeter2(3.0), Meter3(3.0));
+
+    assert_eq!(Meter2(7.0) / PerMeter(2.0), Meter3(3.5));
+
+    assert_eq!(PerMeter(1.2) * Meter(2.0), 2.4);
+    assert_eq!(PerMeter(2.2) * Meter2(2.0), Meter(4.4));
+    assert_eq!(PerMeter(3.2) * Meter3(2.0), Meter2(6.4));
+
+    assert_eq!(PerMeter2(1.2) * Meter(2.0), PerMeter(2.4));
+    assert_eq!(PerMeter2(2.2) * Meter2(2.0), 4.4);
+    assert_eq!(PerMeter2(3.2) * Meter3(2.0), Meter(6.4));
+
+    assert_eq!(PerMeter3(1.2) * Meter(2.0), PerMeter2(2.4));
+    assert_eq!(PerMeter3(2.2) * Meter2(2.0), PerMeter(4.4));
+    assert_eq!(PerMeter3(3.2) * Meter3(2.0), 6.4);
+
+    assert_eq!(PerMeter(12.0) / Meter(6.0), PerMeter2(2.0));
+    assert_eq!(PerMeter(12.0) / Meter2(3.0), PerMeter3(4.0));
+
+    assert_eq!(PerMeter2(12.0) / Meter(4.0), PerMeter3(3.0));
+
+    assert_eq!(PerMeter(18.0) / PerMeter(6.0), 3.0);
+    assert_eq!(PerMeter(18.0) / PerMeter2(2.0), Meter(9.0));
+    assert_eq!(PerMeter(20.0) / PerMeter3(5.0), Meter2(4.0));
+
+    assert_eq!(PerMeter2(20.0) / PerMeter(10.0), PerMeter(2.0));
+    assert_eq!(PerMeter2(24.0) / PerMeter2(6.0), 4.0);
+    assert_eq!(PerMeter2(24.0) / PerMeter3(12.0), Meter(2.0));
+
+    assert_eq!(PerMeter3(3.0) / PerMeter(3.0), PerMeter2(1.0));
+    assert_eq!(PerMeter3(6.0) / PerMeter2(2.0), PerMeter(3.0));
+    assert_eq!(PerMeter3(30.0) / PerMeter3(3.0), 10.0);
 }
 
 #[test]
